00d3aec088aa9f322352aa95f6666ec83a30f1de
kinblandford <nawper02@gmail.com>
2023-12-05 17:01:25 -0700

    Functioning CLI input area and title print. Also have input buffer and some basic commands. Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // blang.rs is a command line rpn calculator.
 
-// crossterm gives us control over the console.
 use crossterm::{
     execute,
     style::{Color, Print, SetBackgroundColor, SetForegroundColor, SetAttribute, Attribute},
@@ -9,12 +8,47 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, size},
     ExecutableCommand,
 };
-use std::io::{stdout, Write, Stdout};
+use std::io::{stdout, Stdout};
 
 enum Format {
     Bold,
     Italic,
     Underlined,
+}
+
+enum AppMode {
+    Insert,
+    Command,
+    // Other modes can be added here
+}
+
+enum LoopControl {
+    Continue,
+    Break,
+}
+
+struct TerminalSize {
+    cols: u16,
+    rows: u16,
+}
+
+impl TerminalSize {
+    fn new() -> TerminalSize {
+        let (cols, rows) = size().unwrap();
+        TerminalSize { cols, rows }
+    }
+
+    fn update(&mut self, new_cols: u16, new_rows: u16) {
+        self.cols = new_cols;
+        self.rows = new_rows;
+    }
+}
+
+struct AppContext {
+    input_buffer: String,
+    terminal_size: TerminalSize,
+    current_mode: AppMode,
+    // ... other fields ...
 }
 
 fn main() {
@@ -26,16 +60,21 @@ fn main() {
     // hide the cursor
     stdout.execute(Hide).unwrap();
 
-    // input buffer
-    let mut input_buffer = String::new();
+    // initialize app context
+    let mut context = AppContext {
+        input_buffer: String::new(),
+        terminal_size: TerminalSize::new(),
+        current_mode: AppMode::Insert,
+        // ... other initializations ...
+    };
 
     // enter user input loop
-    input_loop(&mut input_buffer, &mut stdout);
+    input_loop(&mut context, &mut stdout);
 
     // clean up
     disable_raw_mode().unwrap();
     stdout.execute(Show).unwrap(); // Show the cursor again
-    println!("\nExited.");
+    println!("\nblang done. thank you.");
 }
 
 fn print_formatted_at(stdout: &mut Stdout, text: &str, formats: &[Format], x: u16, y: u16) {
@@ -51,9 +90,9 @@ fn print_formatted_at(stdout: &mut Stdout, text: &str, formats: &[Format], x: u1
     execute!(stdout, SetAttribute(Attribute::Reset)).unwrap();
 }
 
-fn update_input_area(stdout: &mut Stdout, input_buffer: &String, cols: u16, rows: u16) {
+fn update_input_area(stdout: &mut Stdout, input_buffer: &String, terminal_size: &TerminalSize) {
     // Clear the bottom line
-    execute!(stdout, MoveTo(0, rows - 1), Clear(ClearType::CurrentLine)).unwrap();
+    execute!(stdout, MoveTo(0, terminal_size.rows - 1), Clear(ClearType::CurrentLine)).unwrap();
 
     // Set background and foreground colors for the bottom line
     execute!(
@@ -63,12 +102,12 @@ fn update_input_area(stdout: &mut Stdout, input_buffer: &String, cols: u16, rows
     ).unwrap();
 
     // Fill the bottom line with spaces to create a white background
-    let blank_line = " ".repeat(cols as usize);
+    let blank_line = " ".repeat(terminal_size.cols as usize);
     execute!(stdout, Print(&blank_line)).unwrap();
 
     // Move the cursor back to the beginning of the line and print the input buffer
-    execute!(stdout, MoveTo(0, rows - 1)).unwrap();
-    let padded_input = format!(" {} ", input_buffer); // Adding space before and after the text
+    execute!(stdout, MoveTo(0, terminal_size.rows - 1)).unwrap();
+    let padded_input = format!(" {} ", input_buffer);
     execute!(stdout, Print(&padded_input)).unwrap();
 
     // Reset colors
@@ -79,57 +118,52 @@ fn update_input_area(stdout: &mut Stdout, input_buffer: &String, cols: u16, rows
     ).unwrap();
 }
 
-fn process_event(event: Event, input_buffer: &mut String, stdout: &mut Stdout, cols: u16, rows: u16) -> (bool, u16, u16) {
+fn process_event(event: Event, context: &mut AppContext, stdout: &mut Stdout) -> LoopControl {
     match event {
         Event::Key(key_event) => {
             match key_event.code {
-                KeyCode::Char('q') => return (false, cols, rows),
-                KeyCode::Backspace => {input_buffer.pop();},
+                KeyCode::Char('q') => return LoopControl::Break,
+                KeyCode::Backspace => { context.input_buffer.pop(); },
                 KeyCode::Enter => {
-                    //process_buffer(input_buffer);
-                    input_buffer.clear();
+                    //process_buffer(&context.input_buffer);
+                    context.input_buffer.clear();
                 },
-                KeyCode::Char(c) => {input_buffer.push(c);},
+                KeyCode::Char(c) => { context.input_buffer.push(c); },
                 _ => {},
             }
-            (true, cols, rows) // Continue the loop
         },
         Event::Resize(new_cols, new_rows) => {
-            update_graphics(stdout, input_buffer, new_cols, new_rows);
-            (true, new_cols, new_rows) // New dimensions
+            context.terminal_size.update(new_cols, new_rows);
+            update_graphics(stdout, context);
         },
-        _ => (true, cols, rows), // Continue the loop with current dimensions
+        _ => {},
     }
+    LoopControl::Continue
 }
 
-fn update_graphics(stdout: &mut Stdout, input_buffer: &String, cols: u16, rows: u16) {
+fn update_graphics(stdout: &mut Stdout, context: &AppContext) {
     // Clear the screen and redraw title and input area
     execute!(stdout, Clear(ClearType::All)).unwrap();
-    let formats = [Format::Bold, Format::Italic]; // Adjust as needed
-    print_formatted_at(stdout, "blang.rs", &formats, cols / 2 - 4, 0); // Center title
-    update_input_area(stdout, input_buffer, cols, rows);
+
+    let formats = [Format::Bold, Format::Italic];
+    print_formatted_at(stdout, "blang.rs", &formats, context.terminal_size.cols / 2 - 4, 0);
+
+    update_input_area(stdout, &context.input_buffer, &context.terminal_size);
 }
 
-fn input_loop(input_buffer: &mut String, stdout: &mut Stdout) {
-    let (mut cols, mut rows) = size().unwrap();
+
+fn input_loop(context: &mut AppContext, stdout: &mut Stdout) {
+    update_graphics(stdout, context);
 
     loop {
-
-        update_graphics(stdout, input_buffer, cols, rows);
-
-
-        if poll(std::time::Duration::from_millis(500)).unwrap() {
-
+        if poll(std::time::Duration::from_millis(17)).unwrap() {
             let event = read().unwrap();
-            let (continue_loop, new_cols, new_rows) = process_event(event, input_buffer, stdout, cols, rows);
-
-            if !continue_loop {
-                break;
+            match process_event(event, context, stdout) {
+                LoopControl::Continue => {
+                    update_graphics(stdout, context);
+                },
+                LoopControl::Break => break,
             }
-
-            // Update cols and rows with possibly new dimensions
-            cols = new_cols;
-            rows = new_rows;
         }
     }
 }
