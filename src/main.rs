1206965f72e0d85126a4c5e09eb9d66f8296002c
kinblandford <nawper02@gmail.com>
2023-12-05 18:28:40 -0700

    Got rid of chatgpts dumb control flow, added smart control flow instead. Everything working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // blang.rs is a command line rpn calculator.
+// next: border, prettify, stack view, commands, modify how commands are handled to allow for function inputs (not taking immedietly)
 
 #![allow(dead_code)]
 #![allow(unused)]
@@ -25,6 +26,7 @@ enum AppMode {
     Command,
 }
 
+#[derive(PartialEq)]
 enum LoopControl {
     Continue,
     Break,
@@ -49,35 +51,29 @@ impl TerminalSize {
 
 struct AppContext {
     input_buffer: String,
-    command_buffer: String,  // New buffer for command mode
+    command_buffer: String,
     terminal_size: TerminalSize,
     current_mode: AppMode,
+    should_quit: LoopControl,
 }
-
 
 fn main() {
     let mut stdout = stdout();
-
-    // enable raw mode
     enable_raw_mode().unwrap();
-
-    // hide the cursor
     stdout.execute(Hide).unwrap();
 
-    // initialize app context
     let mut context = AppContext {
         input_buffer: String::new(),
         command_buffer: String::new(),
         terminal_size: TerminalSize::new(),
         current_mode: AppMode::Command,
+        should_quit: LoopControl::Continue,
     };
 
-    // enter user input loop
     input_loop(&mut context, &mut stdout);
 
-    // clean up
     disable_raw_mode().unwrap();
-    stdout.execute(Show).unwrap(); // Show the cursor again
+    stdout.execute(Show).unwrap();
     println!("\nblang done. thank you.");
 }
 
@@ -95,64 +91,61 @@ fn print_formatted_at(stdout: &mut Stdout, text: &str, formats: &[TextFormat], x
 }
 
 fn update_input_area(stdout: &mut Stdout, context: &AppContext) {
-    // Clear the bottom line
     execute!(stdout, MoveTo(0, context.terminal_size.rows - 1), Clear(ClearType::CurrentLine)).unwrap();
+    execute!(stdout, SetBackgroundColor(Color::White), SetForegroundColor(Color::Black)).unwrap();
 
-    // Set background and foreground colors for the bottom line
-    execute!(
-        stdout,
-        SetBackgroundColor(Color::White),
-        SetForegroundColor(Color::Black)
-    ).unwrap();
-
-    // Determine which buffer to display based on the current mode
     let buffer_to_display = match context.current_mode {
         AppMode::Insert => &context.input_buffer,
         AppMode::Command => &context.command_buffer,
     };
 
-    // Fill the bottom line with spaces to create a white background
     let blank_line = " ".repeat(context.terminal_size.cols as usize);
     execute!(stdout, Print(&blank_line)).unwrap();
 
-    // Move the cursor back to the beginning of the line and print the appropriate buffer
     execute!(stdout, MoveTo(0, context.terminal_size.rows - 1)).unwrap();
     let padded_input = format!(" {} ", buffer_to_display);
     execute!(stdout, Print(&padded_input)).unwrap();
 
-    // Reset colors
-    execute!(
-        stdout,
-        SetBackgroundColor(Color::Reset),
-        SetForegroundColor(Color::Reset)
-    ).unwrap();
+    execute!(stdout, SetBackgroundColor(Color::Reset), SetForegroundColor(Color::Reset)).unwrap();
 }
 
-fn process_event(event: Event, context: &mut AppContext, stdout: &mut Stdout) -> LoopControl {
+fn process_event(event: Event, context: &mut AppContext, stdout: &mut Stdout) {
     match event {
-        Event::Key(key_event) => match key_event.code {
-            KeyCode::Char('q') if context.current_mode == AppMode::Command => return LoopControl::Break,
-            KeyCode::Esc => context.current_mode = AppMode::Command,
-            KeyCode::Char('i') if context.current_mode == AppMode::Command => context.current_mode = AppMode::Insert,
-            KeyCode::Backspace => match context.current_mode {
-                AppMode::Insert => { context.input_buffer.pop(); },
-                AppMode::Command => { context.command_buffer.pop(); },
-            },
-            KeyCode::Enter => match context.current_mode {
-                AppMode::Insert => {
-                    // process_buffer(&context.input_buffer);
-                    context.input_buffer.clear();
+        Event::Key(key_event) => {
+            match key_event.code {
+                // catch immediete commands
+                KeyCode::Tab => {
+                    context.current_mode = match context.current_mode {
+                        AppMode::Insert => AppMode::Command,
+                        AppMode::Command => AppMode::Insert,
+                    };
                 },
-                AppMode::Command => {
-                    // process_command(&context.command_buffer);
-                    context.command_buffer.clear();
+                KeyCode::Backspace => match context.current_mode {
+                    AppMode::Insert => { context.input_buffer.pop(); },
+                    AppMode::Command => { context.command_buffer.pop(); },
                 },
-            },
-            KeyCode::Char(c) => match context.current_mode {
-                AppMode::Insert => { context.input_buffer.push(c); },
-                AppMode::Command => { context.command_buffer.push(c); },
-            },
-            _ => {},
+                KeyCode::Enter => match context.current_mode {
+                    AppMode::Insert => {
+                        handle_input_buffer(context);
+                        context.input_buffer.clear();
+                    },
+                    AppMode::Command => {
+                        handle_command_buffer(context);
+                        context.command_buffer.clear();
+                    },
+                },
+                // catch word commands or push to buffers
+                KeyCode::Char(c) => match context.current_mode {
+                    // if in insert mode, just push keys to buffer
+                    AppMode::Insert => { context.input_buffer.push(c); },
+                    // if in command mode, push keys to buffer and immedietly handle the buffer
+                    AppMode::Command => {
+                        context.command_buffer.push(c);
+                        handle_command_buffer(context);
+                    },
+                },
+                _ => {},
+            }
         },
         Event::Resize(new_cols, new_rows) => {
             context.terminal_size.update(new_cols, new_rows);
@@ -160,7 +153,25 @@ fn process_event(event: Event, context: &mut AppContext, stdout: &mut Stdout) ->
         },
         _ => {},
     }
-    LoopControl::Continue
+}
+
+fn handle_command_buffer(context: &mut AppContext) {
+    match context.command_buffer.as_str() {
+        "quit" => {
+            context.should_quit = LoopControl::Break;
+        },
+        "clear" => {
+        },
+        "echo" => {
+        },
+        "help" => {
+        },
+        _ => {}
+    }
+}
+
+fn handle_input_buffer(context: &mut AppContext) {
+
 }
 
 fn update_graphics(stdout: &mut Stdout, context: &AppContext) {
@@ -173,7 +184,7 @@ fn update_graphics(stdout: &mut Stdout, context: &AppContext) {
     };
 
     print_formatted_at(stdout, "blang.rs", &formats, context.terminal_size.cols / 2 - 4, 0);
-    print_formatted_at(stdout, mode_text, &formats, 0, context.terminal_size.rows - 2); // Display mode
+    print_formatted_at(stdout, mode_text, &formats, 0, context.terminal_size.rows - 2);
 
     update_input_area(stdout, context);
 }
@@ -184,15 +195,19 @@ fn input_loop(context: &mut AppContext, stdout: &mut Stdout) {
     loop {
         if poll(std::time::Duration::from_millis(17)).unwrap() {
             let event = read().unwrap();
-            match process_event(event, context, stdout) {
+            process_event(event, context, stdout);
+            match context.should_quit {
                 LoopControl::Continue => {
                     update_graphics(stdout, context);
-                },
-                LoopControl::Break => break,
+                }
+                LoopControl::Break => {
+                    break
+                }
             }
         }
     }
 }
+
 
 /*
              @@@@@@@@@@@@@%                        @@@@@@@.
